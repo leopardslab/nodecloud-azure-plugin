410bc229269839f9a5f3bde572a547bcf3ad9690
Rajendra Kadam <rajendrakadam249@gmail.com>
2018-06-01 12:52:52 +0530

    Add azure virtual network

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const virtualmachine = require("./compute/virtual_machine");
 const blobStorage = require("./storage/blob_storage");
 const queueStorage = require("./storage/queue_storage");
 const tableStorage = require("./storage/table_storage");
+const virtualNetwork = require("./network/azure-virtual-network");
 
 class Azure {
   constructor() {
@@ -22,7 +23,8 @@ class Azure {
       compute: this.virtualmachine,
       blob: this.blobstorage,
       queue: this.queuestorage,
-      table: this.tablestorage
+      table: this.tablestorage,
+      network: this.virtualnetwork
     };
   }
 
@@ -40,6 +42,10 @@ class Azure {
 
   tablestorage() {
     return new tableStorage();
+  }
+
+  virtualnetwork() {
+    return new virtualNetwork(this.getSDK());
   }
 }
 
---
+++ network/azure-virtual-network.js
@@ -0,0 +1,97 @@
+const NetworkManagementClient = require("azure-arm-network");
+
+class VirtualNetworks {
+  constructor(azureRestSdk) {
+    this._azureRestSdk = azureRestSdk;
+  }
+
+  create(resourceGroupName, networkName, params) {
+    if (!resourceGroupName || !networkName) {
+      throw new Error("Provide resourceGroupName, networkName");
+    }
+
+    let createPromise = this._azureRestSdk
+      .loginWithServicePrincipalSecret(
+        process.env.AZURE_CLIENT_ID,
+        process.env.AZURE_CLIENT_SECRET,
+        process.env.AZURE_TENANT_ID
+      )
+      .then(credentials => {
+        return new NetworkManagementClient(
+          credentials,
+          process.env.AZURE_SUBSCRIPTION_ID
+        ).virtualNetworks.createOrUpdate(
+          resourceGroupName,
+          networkName,
+          params
+        );
+      });
+
+    return createPromise;
+  }
+
+  delete(resourceGroupName, networkName, params) {
+    if (!networkName) {
+      throw new Error("Provide networkName");
+    }
+
+    let deletePromise = this._azureRestSdk
+      .loginWithServicePrincipalSecret(
+        process.env.AZURE_CLIENT_ID,
+        process.env.AZURE_CLIENT_SECRET,
+        process.env.AZURE_TENANT_ID
+      )
+      .then(credentials => {
+        return new NetworkManagementClient(
+          credentials,
+          process.env.AZURE_SUBSCRIPTION_ID
+        ).virtualNetworks.deleteMethod(resourceGroupName, networkName, params);
+      });
+
+    return deletePromise;
+  }
+
+  get(resourceGroupName, networkName, params) {
+    if (!networkName) {
+      throw new Error("Provide networkName");
+    }
+
+    let getPromise = this._azureRestSdk
+      .loginWithServicePrincipalSecret(
+        process.env.AZURE_CLIENT_ID,
+        process.env.AZURE_CLIENT_SECRET,
+        process.env.AZURE_TENANT_ID
+      )
+      .then(credentials => {
+        return new NetworkManagementClient(
+          credentials,
+          process.env.AZURE_SUBSCRIPTION_ID
+        ).virtualNetworks.get(resourceGroupName, networkName, params);
+      });
+
+    return getPromise;
+  }
+
+  list(resourceGroupName) {
+    if (!resourceGroupName) {
+      throw new Error("Provide resourceGroupName");
+    }
+
+    let listPromise = this._azureRestSdk
+      .loginWithServicePrincipalSecret(
+        process.env.AZURE_CLIENT_ID,
+        process.env.AZURE_CLIENT_SECRET,
+        process.env.AZURE_TENANT_ID
+      )
+      .then(credentials => {
+        return new NetworkManagementClient(
+          credentials,
+          process.env.AZURE_SUBSCRIPTION_ID
+        ).virtualNetworks.list(resourceGroupName);
+      });
+
+    return listPromise;
+  }
+}
+
+module.exports = VirtualNetworks;
